d7d78d2281c8c6d7165feef74df103f1019f7632
dkamaux-pixel <dkamaux@gmail.com>
2026-09-18 20:08:29 +0300

    Update main.jsx

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+function App() {
+  return (
+    <div>
+      <h1>DomyniceTraders AI</h1>
+      <p>Deriv AI Trading Analysis Platform</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
