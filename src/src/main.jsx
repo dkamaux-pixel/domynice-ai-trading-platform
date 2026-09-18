3b9f7f1e2a472fa69ddb5076262f7a40e9e0a9e9
dkamaux-pixel <dkamaux@gmail.com>
2026-09-18 20:13:58 +0300

    Refactor App component into separate file
    Removed the App component definition and imported it from a separate file.

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div>
-      <h1>DomyniceTraders AI</h1>
-      <p>Deriv AI Trading Analysis Platform</p>
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
